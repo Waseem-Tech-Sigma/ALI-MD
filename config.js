15001cca6168d9638ff0a78d2caa7dd686da77e3
Waseem-Tech-yt <waseemsigmaxd@gmail.com>
2025-07-09 06:52:23 +0500

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",  
+SESSION_ID: process.env.SESSION_ID || "ALI-MD~DcUzgKYa#KHYyqwTta_07uYYtRsXyuCHsXcqty2ZcnezXh8NWCCQ",  
 //chatbot integration 
 CHATBOT: process.env.CHATBOT || "on",
 // add your Session Id 
